ca43c3b5bbab1310e99b1e335ec87970cfe047ca
SanAdams <sebastien1adam@gmail.com>
2025-01-28 17:20:32 -0800

    added constructor

--- src/Station.py
+++ src/Station.py
@@ -1,7 +1,14 @@
 from Charger import Charger
 
 class Station:
-    Chargers: list[Charger]
 
-    def uptime():
+    def __init__(self, id: int):
+        self.id = id
+        self.uptime = 0
+        self.Chargers: list[Charger] = [] 
+        
+    def update_uptime(self):
         ...
+
+    def add_charger(self, charger: Charger):
+        self.Chargers.append(charger)
